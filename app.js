7968b1860f54e63dbadb231f1dcc80e3ba131365
kgtkr <contact@kgtkr.net>
2022-01-01 18:00:43 +0900

    discordに投稿する内容を整形

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const { SMTPServer } = require("smtp-server");
 const axios = require("axios");
+const { simpleParser } = require("mailparser");
 
 const server = new SMTPServer({
   secure: false,
   disabledCommands: ["AUTH", "STARTTLS"],
-  onData(stream, session, callback) {
-    const chunks = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("end", async () => {
-      const data = Buffer.concat(chunks).toString("utf8");
-      try {
-        await axios.post(process.env.DISCORD_WEBHOOK, {
-          content: "```" + data + "```",
-        });
-      } catch (e) {
-        console.error(e);
-      }
-      callback();
-    });
+  async onData(stream, session, callback) {
+    const mail = await simpleParser(stream);
+    try {
+      await axios.post(process.env.DISCORD_WEBHOOK, {
+        username: "smtp2discord",
+        content: "New mail received",
+        embeds: [
+          {
+            title: mail.subject,
+            description: mail.text,
+            fields: Array.from(mail.headers).map(([key, value]) => ({
+              name: key,
+              value: value,
+              inline: true,
+            })),
+          },
+        ],
+      });
+    } catch (e) {
+      console.error(e);
+    }
+    callback();
   },
 });
 server.listen(process.env.PORT || 587);
